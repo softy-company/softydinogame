7ac597051048cb720ef91a0e8c1ea7822ea2bd5a
softy-company <vkserfingazi@gmail.com>
2023-10-01 15:00:30 +0600

    finish

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
+const button = document.querySelector('.btn');
 
 
 
-document.addEventListener("keydown", function(event) {
+document.addEventListener("click", function() {
     jump();
 });
 
@@ -14,15 +15,15 @@ function jump() {
     }
     setTimeout(function() {
         dino.classList.remove("jump")
-    }, 300)
+    }, 300)         
  }
-
 
     let is = setInterval(function() {
         let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
         let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
 
         if(cactusLeft < 50 && cactusLeft>0  && dinoTop >= 140){
-            alert("softy крут, а ты лох!")
+            alert(' softy крут, а ты лох!',
+            )
         }
     }, 10)
